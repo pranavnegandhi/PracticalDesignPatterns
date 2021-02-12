ed8b3b24bfea11defa7497e88fd74405c48ce843
Pranav Negandhi <pranav@vertika.tv>
2021-02-12 19:44:59 +0530

    Added adapter example project

---
+++ Adapter/DiskInfoProvider.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Management;
+using System.Threading.Tasks;
+
+namespace Adapter
+{
+    public class DiskInfoProvider
+    {
+        public Task<IEnumerable<PropertyDataCollection>> CollectAsync()
+        {
+            var tcs = new TaskCompletionSource<IEnumerable<PropertyDataCollection>>();
+            var watcher = new ManagementOperationObserver();
+            var results = new List<PropertyDataCollection>();
+
+            var readyHandler = new ObjectReadyEventHandler((sender, e) =>
+            {
+                results.Add(e.NewObject.Properties);
+            });
+
+            // The event handler that gets invoked by the watcher.
+            var completedHandler = default(CompletedEventHandler);
+            completedHandler = new CompletedEventHandler((sender, e) =>
+            {
+                var tcsLocal = tcs;
+                try
+                {
+                    if (e.Status == ManagementStatus.NoError)
+                    {
+                        // The operation was completed without any errors.
+                        tcsLocal.SetResult(results);
+                        return;
+                    }
+
+                    if (e.Status == ManagementStatus.CallCanceled || e.Status == ManagementStatus.OperationCanceled)
+                    {
+                        // The task was cancelled before it could be completed.
+                        tcsLocal.SetCanceled();
+                        return;
+                    }
+
+                    // An exception occurred during the operation.
+                    tcsLocal.SetException(new Exception($"Runtime error {e.Status}"));
+                    return;
+                }
+                finally
+                {
+                    // Clean up the event handlers.
+                    watcher.Completed -= completedHandler;
+                }
+            });
+
+            // Wire up the event handler and begin the operation.
+            watcher.ObjectReady += readyHandler;
+            watcher.Completed += completedHandler;
+            var management = new ManagementClass("Win32_LogicalDisk");
+            management.GetInstances(watcher);
+
+            return tcs.Task;
+        }
+    }
+}
---
+++ Adapter/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Adapter
+{
+    internal class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var provider = new DiskInfoProvider();
+            var results = await provider.CollectAsync();
+
+            foreach (var r in results)
+            {
+                foreach (var p in r)
+                {
+                    Console.WriteLine($"{p.Name}: {p.Value}");
+                }
+                Console.WriteLine("\r");
+            }
+        }
+    }
+}
